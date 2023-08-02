bedde7d40e67c8b9c07ae1012c509ee800a8bccc
kitakou0313 <kitakou0313@gmail.com>
2023-08-03 05:53:25 +0900

    Implement test app with fib

--- main.go
+++ main.go
@@ -1,35 +1,59 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"context"
+	"go-tracing/cmd/app"
+	"log"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	e := echo.New()
+	// e := echo.New()
 
-	e.GET("/service-a-endpoint", func(c echo.Context) error {
-		url := "http://service-b:8080/service-b-endpoint"
+	// e.GET("/service-a-endpoint", func(c echo.Context) error {
+	// 	url := "http://service-b:8080/service-b-endpoint"
 
-		for i := 0; i < 10; i++ {
-			resp, err := http.Get(url)
-			if err != nil {
-				e.Logger.Error(err.Error())
-				return c.String(http.StatusInternalServerError, "Error:"+err.Error())
-			}
-			defer resp.Body.Close()
+	// 	for i := 0; i < 10; i++ {
+	// 		resp, err := http.Get(url)
+	// 		if err != nil {
+	// 			e.Logger.Error(err.Error())
+	// 			return c.String(http.StatusInternalServerError, "Error:"+err.Error())
+	// 		}
+	// 		defer resp.Body.Close()
 
-			byteArray, _ := ioutil.ReadAll(resp.Body)
-			e.Logger.Info((string(byteArray))) // htmlをstringで取得
+	// 		byteArray, _ := ioutil.ReadAll(resp.Body)
+	// 		e.Logger.Info((string(byteArray))) // htmlをstringで取得
+	// 	}
+
+	// 	return c.String(http.StatusOK, "Hello from service-a!")
+	// })
+	// e.GET("/service-b-endpoint", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello from service-b!")
+	// })
+
+	// e.Logger.Fatal(e.Start(":8080"))
+
+	l := log.New(os.Stdout, "", 0)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	errCh := make(chan error)
+
+	app := app.NewApp(os.Stdin, l)
+
+	go func() {
+		errCh <- app.Run(context.Background())
+	}()
+
+	select {
+	case <-sigCh:
+		l.Println("\nGoodBye")
+		return
+	case err := <-errCh:
+		if err != nil {
+			l.Fatal(err)
 		}
-
-		return c.String(http.StatusOK, "Hello from service-a!")
-	})
-	e.GET("/service-b-endpoint", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello from service-b!")
-	})
-
-	e.Logger.Fatal(e.Start(":8080"))
+	}
 }
