efe41daec6d7c14c444e90c7de734282ccc3ea56
kitakou0313 <kitakou0313@gmail.com>
2023-08-04 00:20:01 +0000

    Instrumentation go app

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+
+	"go.opentelemetry.io/otel"
 )
+
+const name = "fib"
 
 type App struct {
 	r io.Reader
@@ -21,16 +25,23 @@ func NewApp(r io.Reader, l *log.Logger) *App {
 
 func (a *App) Run(ctx context.Context) error {
 	for {
-		n, err := a.Poll(ctx)
+		newCtx, span := otel.Tracer(name).Start(ctx, "Run")
+
+		n, err := a.Poll(newCtx)
 		if err != nil {
+			span.End()
 			return err
 		}
 
-		a.WriteNthFibNum(ctx, n)
+		a.WriteNthFibNum(newCtx, n)
+		span.End()
 	}
 }
 
 func (a *App) Poll(ctx context.Context) (uint, error) {
+	_, span := otel.Tracer(name).Start(ctx, "Poll")
+	defer span.End()
+
 	a.l.Print(
 		"What Fib Number would you like to know:",
 	)
@@ -41,7 +52,16 @@ func (a *App) Poll(ctx context.Context) (uint, error) {
 }
 
 func (a *App) WriteNthFibNum(ctx context.Context, n uint) {
-	f, err := Fib(n)
+	ctx, span := otel.Tracer(name).Start(ctx, "WriteNthFibNum")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (uint64, error) {
+		_, span := otel.Tracer(name).Start(ctx, "Fib")
+		defer span.End()
+
+		return Fib(n)
+	}(ctx)
+
 	if err != nil {
 		a.l.Printf("Fibonacci(%d): %v\n", n, err)
 	}
